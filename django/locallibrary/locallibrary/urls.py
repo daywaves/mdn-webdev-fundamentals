1cb474b5fe2226e9881309fe43049e30073227d2
daywaves <daywaves7@gmail.com>
2017-06-17 19:50:33 -0400

    Start django authentication (part 8)

--- django/locallibrary/locallibrary/urls.py
+++ django/locallibrary/locallibrary/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^catalog/', include('catalog.urls')),
-    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True))
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
+    url(r'^accounts/', include('django.contrib.auth.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
