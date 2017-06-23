8393afea1149c804797ca35b7e373fbfa8d3010c
daywaves <daywaves7@gmail.com>
2017-06-23 03:14:53 -0400

    Complete django forms (part 9)

--- django/locallibrary/catalog/views.py
+++ django/locallibrary/catalog/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Book, Author, BookInstance, Genre
 from .forms import RenewBookModelForm
@@ -112,3 +114,40 @@ def renew_book_librarian(request, pk):
         form = RenewBookModelForm(initial={'renewal_date': proposed_renewal_date, })
 
     return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst': book_inst})
+
+
+class AuthorCreate(CreateView, PermissionRequiredMixin):
+    model = Author
+    fields = '__all__'
+    initial = {'date_of_death': '12/31/2999', }
+    permission_required = 'catalog.can_mark_returned'
+
+
+class AuthorUpdate(UpdateView, PermissionRequiredMixin):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    permission_required = 'catalog.can_mark_returned'
+
+
+class AuthorDelete(DeleteView, PermissionRequiredMixin):
+    model = Author
+    success_url = reverse_lazy('authors')
+    permission_required = 'catalog.can_mark_returned'
+
+
+class BookCreate(CreateView, PermissionRequiredMixin):
+    model = Book
+    fields = '__all__'
+    permission_required = 'catalog.can_mark_returned'
+
+
+class BookUpdate(UpdateView, PermissionRequiredMixin):
+    model = Book
+    fields = '__all__'
+    permission_required = 'catalog.can_mark_returned'
+
+
+class BookDelete(DeleteView, PermissionRequiredMixin):
+    model = Book
+    success_url = reverse_lazy('books')
+    permission_required = 'catalog.can_mark_returned'
